836de04937ba480769f4a34223d13e91b29de7e8
Michael Wismont <m.wismont@gwccnet.com>
2022-10-26 10:30:47 -0500

    Adding more code smells

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,6 +10,29 @@ export class Tab2Page {
   constructor() {
     let msg = 'Hello';
     'World';
+
+    this.looperTest(10);
+
+      let req = {
+        'cc': '1234-1234-1234-1234',
+        'exp': '2022-12-31',
+        'cvv': '144'
+      }
+
+      let CCNumber = req.cc
+      let expDate = req.exp
+      let CVV = req.cc
+      let orderNumber = 'abc123'
+      let success = this.processPayment(CCNumber, expDate, CVV, orderNumber)
+      console.log('payment info: ' + JSON.stringify(req))
   }
 
+  looperTest(i) {
+    for (let count=0; count < i; i++) {
+      console.log('here: ' + count);
+    }
+  }
+
+  processPayment(CCNumber, expDate, CVV, orderNumber) {
+  }
 }
